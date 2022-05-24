6393903b73f3346a1920d26f64e06ec39078e2a4
ahamedzoha <ahamed.zoha@gmail.com>
2022-05-24 23:11:04 +0600

    added conditional color and hover effect and fixed responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        up: "#31DB7F",
+        down: "#FF0000",
+        equal: "#67CBF5",
+      },
+    },
   },
   plugins: [],
 }
