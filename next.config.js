47eff9b7b4fb67699ee29d0b61b852b475a743e0
ahamedzoha <ahamed.zoha@gmail.com>
2022-07-24 00:37:04 +0600

    initialized next with tailwind + cleanup 🧹🧼

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-};
+  swcMinify: true,
+}
+
+module.exports = nextConfig
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-slate-600">
+      <Head></Head>
+
+      <main className=""></main>
+
+      <footer className=""></footer>
+    </div>
+  )
+}
