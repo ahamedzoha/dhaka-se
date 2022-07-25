effb9f8570f39c8cd3bb73b18dbcc8fe5436bbef
ahamedzoha <ahamed.zoha@gmail.com>
2022-07-26 00:59:44 +0600

    conditionally rendering sitenav in layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,15 +1,35 @@
 import Head from "next/head"
 import React, { Fragment } from "react"
 import SiteNav from "./sitenav"
+import { useRouter } from "next/router"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter()
+  const { pathname } = router
+  const isDashboard = pathname === "/dashboard"
+
+  const publicRoute = [
+    { name: "Home", href: "/" },
+    { name: "Features", href: "/features" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" },
+  ]
+
+  const privateRoute = [
+    { name: "Dashboard", href: "/dashboard" },
+    { name: "Logout", href: "/logout" },
+  ]
+
+  const isPrivateRoute = privateRoute.some((item) => item.href === pathname)
+  const isPublicRoute = publicRoute.some((item) => item.href === pathname)
+
   return (
     <Fragment>
       <Head>
         <title>DSE Watch</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <SiteNav />
+      {isPrivateRoute ? null : <SiteNav />}
       {children}
     </Fragment>
   )
--- components/layout/sitenav.tsx
+++ components/layout/sitenav.tsx
@@ -25,14 +25,14 @@ const SiteNav = () => {
           <div className="flex items-center flex-1">
             <div className="flex items-center justify-between w-full md:w-auto">
               <Link href="/">
-                <>
+                <a className="cursor-pointer">
                   <span className="sr-only">Workflow</span>
                   <CustomImage
                     className="h-8 w-auto sm:h-10"
                     src={workflow2}
                     alt=""
                   />
-                </>
+                </a>
               </Link>
               <div className="-mr-2 flex items-center md:hidden">
                 <Popover.Button className="bg-gray-800 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
