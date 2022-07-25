2a083465436a77543caa64d6eb38a5157dc69eac
ahamedzoha <ahamed.zoha@gmail.com>
2022-07-25 15:49:07 +0600

    updated layout

---
+++ components/layout/layout.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head"
+import React, { Fragment } from "react"
+
+/**
+ * @author ahamedzoha
+ */
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>DSE Watch</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {children}
+    </Fragment>
+  )
+}
+export default Layout
