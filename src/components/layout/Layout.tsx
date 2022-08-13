a00aa7eea848589ba4d45f7b4ece4300e1307d4f
ahamedzoha <ahamed.zoha@gmail.com>
2022-08-14 03:46:06 +0600

    style: added page transitions 🎉

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { AppProps } from 'next/app'
 import React from 'react'
 
@@ -14,15 +14,18 @@ export default function Layout({ children, router }: LayoutProps) {
   return (
     <>
       <Header />
-      <motion.div
-        key={router.route}
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
-        className=' mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'
-      >
-        {children}
-      </motion.div>
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className=' mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'
+          exit={{ opacity: 0, scale: 0.9 }}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 import Seo from '@/components/Seo'
 
@@ -14,18 +15,25 @@ import HeroGraphDark from '~/svg/graph-dark.svg'
 import HeroGraphLight from '~/svg/graph-light.svg'
 
 export default function HomePage() {
+  const [mounted, setMounted] = useState<boolean>(false)
   const { resolvedTheme } = useTheme()
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
   return (
     <>
       <Seo templateTitle='Home' />
 
       <main className=''>
         {/* Hero Container */}
-        <div className='mt-40 flex  w-full flex-wrap items-center justify-center'>
+        <div className='mt-20 flex w-full  flex-wrap items-center justify-center lg:mt-40'>
           {/* Text */}
           <div className='flex flex-col justify-center lg:w-6/12'>
-            <h1 className='max-w-xl text-7xl'>Track the Market</h1>
-            <p className='mt-8 max-w-lg text-3xl'>
+            <h1 className='max-w-xl text-5xl md:text-7xl'>Track the Market</h1>
+            <p className='mt-8 max-w-lg text-2xl md:text-3xl'>
               Manage your <strong>DSE</strong> portfolio and track your
               investments in a simple and easy way.
             </p>
