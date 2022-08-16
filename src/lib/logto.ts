c109ce4846e49b225b89f89727a851136d7b109a
ahamedzoha <ahamed.zoha@gmail.com>
2022-08-16 15:44:30 +0600

    fix: updated logto config

--- src/lib/logto.ts
+++ src/lib/logto.ts
@@ -4,6 +4,7 @@ import LogtoClient from '@logto/next'
 export const logtoClient = new LogtoClient({
   endpoint: process.env.LOGTO_ENDPOINT!,
   appId: process.env.LOGTO_APPID!,
+  appSecret: process.env.LOGTO_APP_SECRET!,
   baseUrl: process.env.LOGTO_BASEURL!, // Change to your own base URL
   cookieSecret: process.env.LOGTO_COOKIE_SECRET!, // Auto-generated 32 digit secret
   cookieSecure: process.env.NODE_ENV === 'production',
